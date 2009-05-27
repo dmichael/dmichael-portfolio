39d6dc09f5fdd52eb1f34dd3f4156a72fb1ef383
David M Michael <david@unnature.net>
2009-05-26 21:15:50 -0400

    man o man

--- docs.rb
+++ docs.rb
@@ -53,7 +53,7 @@ helpers do
       end
     rescue
     end
-    @last_commit = File.open(File.dirname(__FILE__) + "/log/commit.log").readlines
+    @last_commit = File.read("#{options.root}/log/commit.log")
     source = File.read(topic_file(topic))
     @content = markdown(source)
     @title, @content = title(@content)
